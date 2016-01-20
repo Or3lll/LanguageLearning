f105b2404e3cc19d31f3fc320f7556422e44e917
Or3lll <or3lll@gmail.com>
2016-01-20 11:44:28 +0100

    Configuration of Toolbar in languages list Activity: title and and up button

--- app/src/main/java/net/or3lll/languagelearning/LangListActivity.java
+++ app/src/main/java/net/or3lll/languagelearning/LangListActivity.java
@@ -1,8 +1,10 @@
 package net.or3lll.languagelearning;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import net.or3lll.languagelearning.configuration.lang.AddLangActivity;
 import net.or3lll.languagelearning.data.Lang;
@@ -13,6 +15,13 @@ public class LangListActivity extends AppCompatActivity implements LangListFragm
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lang_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle(R.string.title_activity_languages);
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
