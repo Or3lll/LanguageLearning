bef9a039f4930985b561d3681e5924ec29ae01fb
Or3lll <or3lll@gmail.com>
2016-03-09 09:50:31 +0100

    Update test to use translation in database, close #7

--- app/src/main/java/net/or3lll/languagelearning/VocabularyTestFragment.java
+++ app/src/main/java/net/or3lll/languagelearning/VocabularyTestFragment.java
@@ -10,7 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import net.or3lll.languagelearning.data.Translation;
+
 import java.util.List;
 
 
@@ -21,24 +22,10 @@ import java.util.List;
  */
 public class VocabularyTestFragment extends Fragment {
 
-    public class JpFrWord {
-        public String kanji;
-        public String kana;
-        public String french;
-
-        public JpFrWord(String _kanji, String _kana, String _french) {
-            this.kanji = _kanji;
-            this.kana = _kana;
-            this.french = _french;
-        }
-    }
-
-    private List<JpFrWord> words;
-
-    private TextView kanjiTextView;
-    private TextView kanaTextView;
-    private EditText frEditText;
-    private Button displayKanaBtn;
+    private TextView textTextView;
+    private TextView subTextTextView;
+    private EditText answerEditText;
+    private Button displaySubTextBtn;
     private Button checkBtn;
 
     private int index;
@@ -48,12 +35,6 @@ public class VocabularyTestFragment extends Fragment {
     public VocabularyTestFragment() {
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment VocabularyTestFragment.
-     */
     public static VocabularyTestFragment newInstance() {
         VocabularyTestFragment fragment = new VocabularyTestFragment();
         Bundle args = new Bundle();
@@ -70,12 +51,6 @@ public class VocabularyTestFragment extends Fragment {
                 attempts = savedInstanceState.getInt("tent");
             }
         }
-
-        words = new ArrayList<JpFrWord>();
-
-        words.add(new JpFrWord("教師", "", "Enseignant"));
-        words.add(new JpFrWord("私", "わたし", "Je"));
-        words.add(new JpFrWord("私達", "わたしたち", "Nous"));
     }
 
     @Override
@@ -83,17 +58,17 @@ public class VocabularyTestFragment extends Fragment {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_vocabulary_test, container, false);
 
-        kanjiTextView = (TextView) v.findViewById(R.id.kanji_text);
-        kanaTextView = (TextView) v.findViewById(R.id.kana_text);
-        frEditText = (EditText) v.findViewById(R.id.french_response);
+        textTextView = (TextView) v.findViewById(R.id.text_text);
+        subTextTextView = (TextView) v.findViewById(R.id.subtext_text);
+        answerEditText = (EditText) v.findViewById(R.id.answer_edittext);
 
-        displayKanaBtn = (Button) v.findViewById(R.id.display_kana_btn);
+        displaySubTextBtn = (Button) v.findViewById(R.id.display_subtext_btn);
         checkBtn = (Button) v.findViewById(R.id.check_btn);
 
-        displayKanaBtn.setOnClickListener(new View.OnClickListener() {
+        displaySubTextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                kanaTextView.setVisibility(View.VISIBLE);
+                subTextTextView.setVisibility(View.VISIBLE);
                 return;
             }
         });
@@ -101,14 +76,15 @@ public class VocabularyTestFragment extends Fragment {
         checkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                JpFrWord word = words.get(index);
+                Translation translation = Translation.listAll(Translation.class).get(index);
                 attempts++;
 
-                if (word.french.equalsIgnoreCase(frEditText.getText().toString())) {
+                if (((attempts % 2 == 0) && translation.word1.text.equalsIgnoreCase(answerEditText.getText().toString()))
+                    || ((attempts % 2 == 1) && translation.word2.text.equalsIgnoreCase(answerEditText.getText().toString()))) {
                     score++;
-                    Toast.makeText(VocabularyTestFragment.this.getContext(), "Correct " + score + " / " + attempts, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(VocabularyTestFragment.this.getContext(), String.format(getString(R.string.good_answer), score, attempts), Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(VocabularyTestFragment.this.getContext(), "Faux " + score + " / " + attempts, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(VocabularyTestFragment.this.getContext(), String.format(getString(R.string.bad_answer), score, attempts), Toast.LENGTH_SHORT).show();
                 }
 
                 setWords();
@@ -129,14 +105,22 @@ public class VocabularyTestFragment extends Fragment {
     }
 
     private void setWords() {
-        index = (int) (Math.random() * (double)words.size());
+        List<Translation> translations = Translation.listAll(Translation.class);
+        index = (int) (Math.random() * (double)translations.size());
 
-        JpFrWord word = words.get(index);
+        Translation translation = translations.get(index);
 
-        kanjiTextView.setText(word.kanji);
-        kanaTextView.setText(word.kana);
-        frEditText.setText("");
+        if(attempts % 2 == 0) {
+            textTextView.setText(translation.word1.text);
+            subTextTextView.setText(translation.word1.subText);
+            answerEditText.setText("");
+        }
+        else {
+            textTextView.setText(translation.word2.text);
+            subTextTextView.setText(translation.word2.subText);
+            answerEditText.setText("");
+        }
 
-        kanaTextView.setVisibility(View.INVISIBLE);
+        subTextTextView.setVisibility(View.INVISIBLE);
     }
 }
