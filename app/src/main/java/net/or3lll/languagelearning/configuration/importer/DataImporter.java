895abbff800725fa64562d9b8afa486f1a27e845
Or3lll <or3lll@gmail.com>
2016-09-05 07:29:48 +0200

    Import lang

--- app/src/main/java/net/or3lll/languagelearning/configuration/importer/DataImporter.java
+++ app/src/main/java/net/or3lll/languagelearning/configuration/importer/DataImporter.java
@@ -1,16 +1,72 @@
 package net.or3lll.languagelearning.configuration.importer;
 
+import com.orm.SugarRecord;
+
+import net.or3lll.languagelearning.data.Lang;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by or3lll on 05/09/2016.
  */
 public class DataImporter {
-    public void load(BufferedReader reader) {
+    private List<Lang> langsToAdd = new ArrayList<Lang>();
 
+
+    public void load(BufferedReader reader) {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            String json = sb.toString();
+            analyzeJson(json);
+
+        }
+        catch (IOException e) {
+
+        }
+    }
+
+    private void analyzeJson(String jsonText) {
+        try {
+            JSONObject json = new JSONObject(jsonText);
+            try {
+                JSONArray langs = json.getJSONArray("langs");
+                for (int i = 0; i < langs.length(); i++) {
+                    JSONObject jsonLang = langs.getJSONObject(i);
+                    String isoCode = jsonLang.getString("isoCode");
+                    if(SugarRecord.find(Lang.class, "iso_code=?", isoCode).size() == 0) {
+                        String name = jsonLang.getString("name");
+                        Lang lang = new Lang();
+                        lang.setIsoCode(isoCode);
+                        lang.setName(name);
+                        if(lang.isValid()) {
+                            langsToAdd.add(lang);
+                        }
+                    }
+                }
+            }
+            catch (JSONException e) {
+
+            }
+        }
+        catch (JSONException e) {
+
+        }
     }
 
     public void apply() {
-
+        for (Lang lang : langsToAdd) {
+            SugarRecord.save(lang);
+        }
     }
 }
