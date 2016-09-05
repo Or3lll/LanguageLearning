3b8e1b861a4e84a43adbbeee63dd4c9d1133b879
Or3lll <or3lll@gmail.com>
2016-09-05 14:02:54 +0200

    Import Word

--- app/src/androidTest/java/net/or3lll/languagelearning/data/ImporterTest.java
+++ app/src/androidTest/java/net/or3lll/languagelearning/data/ImporterTest.java
@@ -10,12 +10,14 @@ import com.orm.SugarRecord;
 
 import net.or3lll.languagelearning.configuration.importer.DataImporter;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 /**
  * Created by or3lll on 05/09/2016.
@@ -27,22 +29,66 @@ public class ImporterTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
+    @Before
+    public void prepare() {
+        SugarRecord.deleteAll(Lang.class);
+    }
+
     @Test
     public void importLangsOnly() throws Exception {
+        cleanData();
+
         String dataToImport =
-                "{" +
-                "\"langs\": [{" +
-                "\"isoCode\": \"es_ES\"," +
-                "\"name\": \"Espagnol\"" +
-                "}, {" +
-                "\"isoCode\": \"pt_BR\"," +
-                "\"name\": \"Portugais du Brésil\"" +
-                "}]" +
-                "}";
+            "{" +
+            "\"langs\": [{" +
+            "\"isoCode\": \"es_ES\"," +
+            "\"name\": \"Espagnol\"" +
+            "}, {" +
+            "\"isoCode\": \"pt_BR\"," +
+            "\"name\": \"Portugais du Brésil\"" +
+            "}]" +
+            "}";
+
         DataImporter importer = new DataImporter();
         importer.load(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(dataToImport.getBytes()))));
         importer.apply();
+
         assertEquals(1, SugarRecord.find(Lang.class, "iso_code=?", "es_ES").size());
         assertEquals(1, SugarRecord.find(Lang.class, "iso_code=?", "pt_BR").size());
+    }
+
+    @Test
+    public void importWordsOnly() throws Exception {
+        cleanData();
+
+        SugarRecord.save(new Lang("Français", "fr_FR"));
+
+        String dataToImport =
+            "{" +
+            "\"words\": [{" +
+            "\"isoCode\": \"fr_FR\"," +
+            "\"text\": \"voiture\"," +
+            "\"subText\": \"\"," +
+            "\"desc\": \"\"" +
+            "}, {" +
+            "\"isoCode\": \"fr_FR\"," +
+            "\"text\": \"souris\"," +
+            "\"subText\": \"\"," +
+            "\"desc\": \"Pour ordinateur\"" +
+            "}]" +
+            "}";
+
+        DataImporter importer = new DataImporter();
+        importer.load(new BufferedReader(new InputStreamReader(new ByteArrayInputStream(dataToImport.getBytes()))));
+        importer.apply();
+
+        assertEquals(1, Word.find(Word.class, "text=?", "voiture").size());
+        assertEquals(1, Word.find(Word.class, "text=?", "souris").size());
+    }
+
+    private void cleanData() {
+        Translation.deleteAll(Translation.class);
+        Word.deleteAll(Word.class);
+        SugarRecord.deleteAll(Lang.class);
     }
 }
--- app/src/main/java/net/or3lll/languagelearning/configuration/importer/DataImporter.java
+++ app/src/main/java/net/or3lll/languagelearning/configuration/importer/DataImporter.java
@@ -1,8 +1,11 @@
 package net.or3lll.languagelearning.configuration.importer;
 
 import com.orm.SugarRecord;
+import com.orm.query.Condition;
+import com.orm.query.Select;
 
 import net.or3lll.languagelearning.data.Lang;
+import net.or3lll.languagelearning.data.Word;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,7 +20,8 @@ import java.util.List;
  * Created by or3lll on 05/09/2016.
  */
 public class DataImporter {
-    private List<Lang> langsToAdd = new ArrayList<Lang>();
+    private List<Lang> langsToAdd = new ArrayList<>();
+    private List<Word> wordsToAdd = new ArrayList<>();
 
 
     public void load(BufferedReader reader) {
@@ -46,11 +50,42 @@ public class DataImporter {
                     String isoCode = jsonLang.getString("isoCode");
                     if(SugarRecord.find(Lang.class, "iso_code=?", isoCode).size() == 0) {
                         String name = jsonLang.getString("name");
-                        Lang lang = new Lang();
-                        lang.setIsoCode(isoCode);
-                        lang.setName(name);
+                        Lang lang = new Lang(name, isoCode);
                         if(lang.isValid()) {
                             langsToAdd.add(lang);
+                        }
+                    }
+                }
+            }
+            catch (JSONException e) {
+
+            }
+
+            try {
+                JSONArray jsonWords = json.getJSONArray("words");
+                for (int i = 0; i < jsonWords.length(); i++) {
+                    JSONObject jsonWord = jsonWords.getJSONObject(i);
+                    String isoCode = jsonWord.getString("isoCode");
+
+                    List<Lang> langs = SugarRecord.find(Lang.class, "iso_code=?", isoCode);
+                    if(langs.size() == 1) {
+                        Lang lang = langs.get(0);
+
+                        String text = jsonWord.getString("text");
+                        List<Word> words = Word.find(Word.class, "text=?", text);
+                        boolean isAlreadyExist = false;
+                        for (Word word : words) {
+                            if(word.lang.getId() == lang.getId()) {
+                                isAlreadyExist = true;
+                                break;
+                            }
+                        }
+
+                        if(!isAlreadyExist) {
+                            String subText = jsonWord.getString("subText");
+                            String desc = jsonWord.getString("desc");
+                            Word word = new Word(lang, text, subText, desc);
+                            wordsToAdd.add(word);
                         }
                     }
                 }
@@ -67,6 +102,10 @@ public class DataImporter {
     public void apply() {
         for (Lang lang : langsToAdd) {
             SugarRecord.save(lang);
+        }
+
+        for (Word word : wordsToAdd) {
+            word.save();
         }
     }
 }
