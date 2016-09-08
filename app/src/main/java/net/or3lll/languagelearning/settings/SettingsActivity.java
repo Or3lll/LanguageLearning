6e188c879775bbe0b9365bec4c183f638738e1dc
Or3lll <or3lll@gmail.com>
2016-09-08 13:04:15 +0200

    Add empty settings Activity

---
+++ app/src/main/java/net/or3lll/languagelearning/settings/SettingsActivity.java
@@ -0,0 +1,24 @@
+package net.or3lll.languagelearning.settings;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import net.or3lll.languagelearning.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setTitle("Paramètres");
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
