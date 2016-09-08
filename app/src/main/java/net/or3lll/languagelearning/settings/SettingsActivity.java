daa5c9d3dc03b17413f008f893d806ebe337482f
Or3lll <or3lll@gmail.com>
2016-09-08 23:53:33 +0200

    End of import and export data base, close #11 #22

--- app/src/main/java/net/or3lll/languagelearning/settings/SettingsActivity.java
+++ app/src/main/java/net/or3lll/languagelearning/settings/SettingsActivity.java
@@ -11,10 +11,16 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import com.orm.SugarRecord;
 
 import net.or3lll.languagelearning.R;
 import net.or3lll.languagelearning.configuration.exporter.DataExporter;
 import net.or3lll.languagelearning.configuration.importer.DataImporter;
+import net.or3lll.languagelearning.data.Lang;
+import net.or3lll.languagelearning.data.Translation;
+import net.or3lll.languagelearning.data.Word;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -29,6 +35,9 @@ public class SettingsActivity extends AppCompatActivity {
     private static final String BACK_UP_FILE_NAME = "backup_data.json";
     private static final String BACK_UP_DIRECTORY_NAME = "language_backup";
 
+    public static final int REQUEST_PERMISSION_READ_EXTERNAL_STORAGE = 0;
+    public static final int REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,109 +47,102 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         ActionBar ab = getSupportActionBar();
-        ab.setTitle("Paramètres");
+        ab.setTitle(R.string.title_activity_settings);
         ab.setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case 42: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            case REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     saveData();
                 }
-                return;
-            }
-
-            case 69: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                else {
+                    Toast.makeText(this, R.string.error_no_permission_save, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case REQUEST_PERMISSION_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     restoreData();
                 }
-                return;
-            }
+                else {
+                    Toast.makeText(this, R.string.error_no_permission_restore, Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
     }
 
     public void onSaveDataClick(View v) {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    42);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE);
         }
         else {
             saveData();
         }
     }
 
-    private void saveData() {
-        DataExporter exporter = new DataExporter();
-        String jsonBackUp = exporter.export();
-
-        if(isExternalStorageWritable()) {
-            File backUpDir = getPublicStorageDir();
-            File backUpFile = new File(backUpDir, BACK_UP_FILE_NAME);
-            try {
-                backUpFile.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
-                FileOutputStream fos = new FileOutputStream(backUpFile);
-                fos.write(jsonBackUp.getBytes());
-                fos.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
     public void onRestoreDataClick(View v) {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    69);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_READ_EXTERNAL_STORAGE);
         }
         else {
             restoreData();
         }
     }
 
+    public void onCleanDataClick(View v) {
+        Translation.deleteAll(Translation.class);
+        Word.deleteAll(Word.class);
+        SugarRecord.deleteAll(Lang.class);
+    }
+
+    private void saveData() {
+        if(isExternalStorageWritable()) {
+            File backUpDir = new File(Environment.getExternalStoragePublicDirectory(BACK_UP_DIRECTORY_NAME), BACK_UP_DIRECTORY_NAME);
+            File backUpFile = new File(backUpDir, BACK_UP_FILE_NAME);
+            FileOutputStream fos = null;
+            try {
+                backUpDir.mkdirs();
+                backUpFile.createNewFile();
+
+                DataExporter exporter = new DataExporter();
+                String jsonBackUp = exporter.export();
+
+                fos = new FileOutputStream(backUpFile);
+                fos.write(jsonBackUp.getBytes());
+                fos.close();
+            } catch (IOException e) {
+                Toast.makeText(this, R.string.error_save_backup, Toast.LENGTH_SHORT).show();
+            } finally {
+                if(fos != null) {
+                    try {
+                        fos.close();
+                    } catch (IOException e) { }
+                }
+            }
+        }
+        else {
+            Toast.makeText(this, R.string.error_external_storage_unavailable, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     private void restoreData() {
         if(isExternalStorageReadable()) {
-            File backUpDir = getPublicStorageDir();
+            File backUpDir = new File(Environment.getExternalStoragePublicDirectory(BACK_UP_DIRECTORY_NAME), BACK_UP_DIRECTORY_NAME);
             File backUpFile = new File(backUpDir, BACK_UP_FILE_NAME);
 
-            DataImporter importer = new DataImporter();
             try {
+                DataImporter importer = new DataImporter();
                 importer.load(new BufferedReader(new FileReader(backUpFile)));
                 importer.apply();
             } catch (FileNotFoundException e) {
-                e.printStackTrace();
+                Toast.makeText(this, R.string.error_restore_backup, Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    public void onCleanDataClick(View v) {
-
-    }
-
-    public File getPublicStorageDir() {
-        File file = new File(Environment.getExternalStoragePublicDirectory("BackUp"), BACK_UP_DIRECTORY_NAME);
-        if (!file.mkdirs()) {
-            Log.e("", "Echec création dir");
+        else {
+            Toast.makeText(this, R.string.error_external_storage_unavailable, Toast.LENGTH_SHORT).show();
         }
-        return file;
     }
 
     /* Checks if external storage is available for read and write */
